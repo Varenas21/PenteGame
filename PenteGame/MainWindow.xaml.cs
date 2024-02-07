0d241c40112f56de9f99e19a46aa01f1bf9c493b
7Varenas2 <98408850+7Varenas2@users.noreply.github.com>
2024-02-07 09:52:13 -0700

    Added random grid size and new menu window for different modes

--- PenteGame/MainWindow.xaml.cs
+++ PenteGame/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using PenteGame.images;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,22 +12,68 @@ using System.Windows.Shapes;
 
 namespace PenteGame
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        MainMenu menu = new MainMenu();
         bool isPlayer = true;
-        int p1CapCount;
-        int p2CapCount;
+        int p1CapCount = 0; // Need to initialize values otherwise we get an exception
+        int p2CapCount = 0;
         bool isCPU = false;
+        
 
         public MainWindow()
         {
             InitializeComponent();
+            int count = 1;
+            int gridSize = 0;
+            //if (menu.ClassicButton.IsPressed = true)
+            //{
+            //    gridSize = 19;
+            //}
+            //else if(menu.RandomButton.IsPressed = true)
+            //{
+            //    gridSize = menu.randomGridSize;
+            //}
+            // Making grid
+            RowDefinition rowDefinition = new RowDefinition();
+            ColumnDefinition columnDefinition = new ColumnDefinition();
+
+            rowDefinition.Height = new GridLength(gridSize);
+            columnDefinition.Width = new GridLength(gridSize);
+
+            GridBoard.RowDefinitions.Add(rowDefinition);
+            GridBoard.ColumnDefinitions.Add(columnDefinition);
+        
+            // Adding buttons dynamically
+            for (int column = 0; column < gridSize; column++)
+            {
+                for (int row = 0; row < gridSize; row++)
+                {
+                    Button button = new Button(); // Buttons added
+                    button.Content = count.ToString(); // Testing that it prints properly
+                    button.Name = "Piece" + count.ToString(); // Naming the piece based off the location
+
+                    //button.Style()
+                    // Adding buttons on rows and columns
+                    Grid.SetColumn(button, column);
+                    Grid.SetRow(button, row);
+                    GridBoard.Children.Add(button);
+
+
+                    count++;
+                }
+                
+
+            }
         }
 
-        Button[] buttonRows = new Button[] { };
-        Button[] buttonColumns = new Button[] { };
+        // On Click for the buttons
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            if(b.IsPressed == true) Console.WriteLine("Button clicked");
+        }
+
     }
 }
---
+++ PenteGame/images/MainMenu.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PenteGame.images
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        public int randomGridSize = 0;
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Random_Click(object sender, RoutedEventArgs e)
+        {
+            Random randomNum = new Random();
+            randomGridSize = randomNum.Next(9, 39);
+        }
+
+        private void Quit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Classic_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Instructions_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://www.pente.net/instructions.html");
+        }
+    }
+}
