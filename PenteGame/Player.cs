ee4b49f348fd7f0c7445109dc86a45bf1d2a1deb
7Varenas2 <98408850+7Varenas2@users.noreply.github.com>
2024-02-14 10:02:46 -0700

    Added some game logic

---
+++ PenteGame/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PenteGame
+{
+    internal class Player
+    {
+        // Player Name
+        // Player Score
+        // Player turn
+        String player1, player2;
+
+
+        public Player() 
+        { 
+
+        }
+
+    }
+}
